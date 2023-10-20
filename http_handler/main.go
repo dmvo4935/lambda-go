2d5f300a363b836d6cc0c712a81871f6d9a46e2d
dmvo4935 <dmytro.voznenko@gmail.com>
2023-10-20 16:04:48 +0300

    http_handler: code

--- http_handler/main.go
+++ http_handler/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
@@ -22,7 +23,9 @@ type MyResponse struct {
 var svc *s3.Client
 
 func init() {
-	cfg, err := config.LoadDefaultConfig(context.Background())
+	fmt.Println("Initializing")
+
+	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion("eu-central-1"))
 	if err != nil {
 		log.Fatalf("Unable to load SDK config: %v", err)
 	}
@@ -33,26 +36,46 @@ func (r *MyResponse) String() string {
 	return fmt.Sprintf("{\"bucket\": \"%s\", \"contents\": %s}", r.BucketName, r.Contents)
 }
 
-func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
+func HandleRequest(ctx context.Context, event *MyEvent) (*MyResponse, error) {
 	if event == nil {
 		return nil, fmt.Errorf("received nil event")
 	}
+	fmt.Printf("Event: %+v", event)
 	contents, err := ListBucket(ctx, event.BucketName)
-	response := fmt.Sprintf("%s", MyResponse{BucketName: event.BucketName, Contents: contents})
-	return &response, err
+
+	return &MyResponse{BucketName: event.BucketName, Contents: contents}, err
 }
 
 func ListBucket(ctx context.Context, name string) ([]string, error) {
+	fmt.Println("Trying to list")
 	var results []string
-	params := s3.ListObjectsV2Input{Bucket: &name}
-	objects, err := svc.ListObjectsV2(ctx, &params)
+	params := &s3.ListObjectsV2Input{Bucket: aws.String(name)}
+
+	fmt.Printf("%+v\n", svc)
+	fmt.Printf("%+v\n", *params.Bucket)
+
+	objects, err := svc.ListObjectsV2(ctx, params)
+	fmt.Printf("%+v %v\n\n", &objects, err)
+
+	if err != nil {
+		return nil, err
+	}
 
 	for i := range objects.Contents {
 		results = append(results, *objects.Contents[i].Key)
 	}
+
 	return results, err
 }
 
 func main() {
 	lambda.Start(HandleRequest)
+
+	// event := &MyEvent{BucketName: "sam-hello-world-4952"}
+
+	// if contents, err := HandleRequest(context.TODO(), event); err != nil {
+	// 	fmt.Printf("%v", err)
+	// } else {
+	// 	fmt.Println(*contents)
+	// }
 }
